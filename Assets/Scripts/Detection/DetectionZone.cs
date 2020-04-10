380765dfe2a617728ed396d03b79e1ea6b686a71
Zergonomics <kjjhawk@gmail.com>
2020-04-10 01:08:05 -0400

    Added detection code for ships

---
+++ Assets/Scripts/Detection/DetectionZone.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectionZone : MonoBehaviour
+{
+    [SerializeField]
+    private float scale;
+    [SerializeField]
+    private float timeInBetween;
+    [SerializeField]
+    private float randomIncrease;
+    [SerializeField]
+    private float initialRandomIncrease;
+
+    private bool active = false;
+
+    private Timer timer;
+
+    [SerializeField]
+    private DetectionZoneInfo info;
+
+    private void Awake()
+    {
+        Initialize(info);
+    }
+
+    public void Initialize(DetectionZoneInfo info)
+    {
+        scale = info.scale;
+        timeInBetween = info.timeInBetween;
+        randomIncrease = info.randomIncrease;
+        initialRandomIncrease = info.initialRandomIncrease;
+        Setup();
+    }
+
+    private void Setup()
+    {
+        transform.localScale = new Vector2(scale, scale);
+        timer = gameObject.AddComponent<Timer>();
+        timer.OnComplete += () =>
+        {
+            GetComponent<Collider2D>().enabled = true;
+            active = true;
+            ResetTimer(randomIncrease);
+            GetComponent<SpriteRenderer>().color = Color.black;
+        };
+
+        GetComponent<Collider2D>().enabled = false;
+        ResetTimer(initialRandomIncrease);
+    }
+
+    private void ResetTimer(float max)
+    {
+        timer.SetTime(0);
+        timer.SetMaxTime(timeInBetween + Random.Range(0, max));
+    }
+
+    private void FixedUpdate()
+    {
+        if (active)
+        {
+            GetComponent<Collider2D>().enabled = false;
+            GetComponent<SpriteRenderer>().color = Color.white;
+            active = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Detection/DetectionZoneInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectionZoneInfo : MonoBehaviour
+{
+    [SerializeField]
+    public float scale;
+    [SerializeField]
+    public float timeInBetween;
+    [SerializeField]
+    public float randomIncrease;
+    [SerializeField]
+    public float initialRandomIncrease;
+}
