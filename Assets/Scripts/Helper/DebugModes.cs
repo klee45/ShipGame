f876677327eb71802f605aaec8c19f624c8f0e96
Zergonomics <kjjhawk@gmail.com>
2020-04-12 14:41:37 -0400

    Initial files for debugging configuration

---
+++ Assets/Scripts/Helper/DebugModes.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DebugModes
+{
+    public static readonly bool SHOW_DETECTION_ZONE = true;
+}
