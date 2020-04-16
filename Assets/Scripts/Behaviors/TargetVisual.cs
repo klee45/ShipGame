04d9bd468e4189c88f46da08c0ef0b20ac6c45f3
Zergonomics <kjjhawk@gmail.com>
2020-04-15 23:39:58 -0400

    Fixed a few small behavior tree visualization bugs

---
+++ Assets/Scripts/Behaviors/TargetVisual.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetVisual : MonoBehaviour
+{
+    [SerializeField]
+    private PilotTree pilot;
+
+    private void Start()
+    {
+        GetComponent<SpriteRenderer>().enabled = true;
+    }
+
+    private void Update()
+    {
+        transform.position = pilot.GetTargetPos();
+    }
+}
