645240ec72f9ba5f5df1dc963404fca8493463a9
klee45 <klee23456@gmail.com>
2020-04-21 00:54:51 -0400

    Added a significant number of ships to test large scale team battles.

---
+++ Assets/Scripts/TeamManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamManager : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 randomOffset = new Vector2(0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Transform child = transform.GetChild(i);
+            child.gameObject.layer = gameObject.layer;
+            child.position += new Vector3(
+                Random.Range(-randomOffset[0] / 2, randomOffset[0] / 2),
+                Random.Range(-randomOffset[1] / 2, randomOffset[1] / 2));
+        }
+    }
+}
