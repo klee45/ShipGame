cac1f3b3e39b211bfc42debb3158c233d7986a81
klee45 <klee23456@gmail.com>
2020-06-15 01:13:56 -0400

    Added basis for map boundry

---
+++ Assets/Scripts/Boundry.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundry : MonoBehaviour
+{
+    private List<Ship> outOfBounds;
+    private float radius;
+
+    private void Awake()
+    {
+        radius = GetComponent<CircleCollider2D>().radius;
+        outOfBounds = new List<Ship>();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //outOfBounds.Add(collision.gameObject.GetComponent<Ship>());
+        Debug.Log("Exit T");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //outOfBounds.Remove(collision.gameObject.GetComponent<Ship>());
+    }
+}
