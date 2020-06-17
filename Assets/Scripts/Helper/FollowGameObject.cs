a455ff66bbded103c9a91ce7a05ef05692c741b7
klee45 <klee23456@gmail.com>
2020-06-17 00:58:57 -0400

    Fixed some sprites and changed testing scene to visualize ship lineup

--- Assets/Scripts/Helper/FollowGameObject.cs
+++ Assets/Scripts/Helper/FollowGameObject.cs
@@ -6,6 +6,8 @@ public class FollowGameObject : MonoBehaviour
 {
     [SerializeField]
     private GameObject target;
+    [SerializeField]
+    private float CameraScale = 2;
 
     /**
     void Start()
@@ -21,6 +23,8 @@ public class FollowGameObject : MonoBehaviour
             Vector3 tp = target.transform.position;
             transform.position = new Vector3(tp.x, tp.y, transform.position.z);
             //transform.rotation = target.transform.rotation;
+
+            GetComponent<Camera>().orthographicSize = CameraScale * transform.localScale.x;
         }
     }
 }
