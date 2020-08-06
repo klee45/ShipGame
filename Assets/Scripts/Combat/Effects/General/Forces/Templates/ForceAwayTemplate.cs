c7534a66df94621ef2dba415f732903e1ae96a94
Kevin <klee23456@gmail.com>
2020-08-06 17:02:14 -0400

    Updated heavy rocket weapon to use better force code

---
+++ Assets/Scripts/Combat/Effects/General/Forces/Templates/ForceAwayTemplate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceAwayTemplate : EntityEffectTemplate
+{
+    [SerializeField]
+    private float duration;
+    [SerializeField]
+    private float strength;
+
+    protected override EntityEffect CreateEffect(GameObject obj)
+    {
+        Force f = obj.AddComponent<Force>();
+        Vector3 positionCreation = GetComponentInParent<Entity>().gameObject.transform.position;
+        Vector3 positionEnd = obj.transform.position;
+        Vector2 direction = (positionEnd - positionCreation).normalized;
+        f.Setup(direction * strength, duration, false);
+        return f;
+    }
+}
