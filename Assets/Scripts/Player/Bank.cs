b4a015b5728131d6d8cf1e163f92500b1b6a1456
Kevin <klee23456@gmail.com>
2020-10-24 21:20:53 -0400

    Bank for use for shop

---
+++ Assets/Scripts/Player/Bank.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bank : MonoBehaviour
+{
+    protected int money = 0;
+
+    public int GetTotalMoney()
+    {
+        return money;
+    }
+
+    public virtual void AddMoney(int money)
+    {
+        this.money += money;
+    }
+
+    public virtual bool TryTakeMoney(int cost)
+    {
+        if (this.money >= cost)
+        {
+            this.money -= cost;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/InfiniteBank.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfiniteBank : Bank
+{
+    private void Awake()
+    {
+        this.money = int.MaxValue;
+    }
+
+    public override void AddMoney(int money)
+    {
+    }
+
+    public override bool TryTakeMoney(int cost)
+    {
+        return true;
+    }
+}
