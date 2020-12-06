0183e061342e4ee4a845773631385f77ad88bdcc
Kevin <klee23456@gmail.com>
2020-12-05 23:42:13 -0500

    Removed old unnecessary type in effect dictionary

--- Assets/Scripts/Combat/Effects/EffectDict.cs
+++ Assets/Scripts/Combat/Effects/EffectDict.cs
@@ -672,6 +672,7 @@ public abstract class EffectDict : MonoBehaviour
                 {
                     if (tag == immune)
                     {
+                        Debug.Log(parent.name + " is immune to " + tag.ToString());
                         Destroy(effect);
                         return false;
                     }
